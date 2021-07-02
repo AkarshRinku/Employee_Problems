a25092da8294535edb4756a138e15248d72dada3
AkarshRinku <86792553+AkarshRinku@users.noreply.github.com>
2021-07-03 01:10:36 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EmpWage1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // UC_1EmployeeAttendance.EmpAttendance();
+            // UC_2EmployeeDailyWage.DailyEmployeeWage();
+            //UC_3PartTimeEmployeeWage.PartTimeWage();
+            //UC_4PartTimeEmployeeWageUsingSwitch.PartTimeWageSwitch();
+            // UC_5CalculatingWagesForMonth.Wages_for_month();
+           //UC_6EmployeeWagesUsingWhileloop.EmpWagesfor100hours();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UC_2EmployeeDailyWage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmpWage1
+{
+    class UC_2EmployeeDailyWage
+    {
+
+        public static void DailyEmployeeWage()
+        {
+            //constants
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            //variables
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            //computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                empHrs = 8;
+
+            }
+            else
+            {
+                empHrs = 0;
+
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+
+            Console.WriteLine("Emp Wage : " + empWage);
+        }
+    }
+}
+  
+
